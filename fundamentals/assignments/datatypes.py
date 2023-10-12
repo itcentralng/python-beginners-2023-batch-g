2368abd25b060ece09fdd7aa25fbd6358358315d
muhammad <muhammadnursanusialiyu05>
2023-10-12 15:22:06 +0100

    yesterdays assignment

--- fundamentals/assignments/datatypes.py
+++ fundamentals/assignments/datatypes.py
@@ -1,9 +1,8 @@
 # Q1. Example 2 examples for each of the following:
     # - String
+# Q2. What are the differences between a tuple and a list?
+# Q3. Which other datatype uses the curly brackets?
     # - Tuple
     # - List
     # - Dictionary
     # - Boolean
-
-# Q2. What are the differences between a tuple and a list?
-# Q3. Which other datatype uses the curly brackets?
