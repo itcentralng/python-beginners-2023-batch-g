eac06b42c5244807d65b9e325cdd66e333dedc22
muhammad <muhammadnursanusialiyu05>
2023-10-12 15:20:42 +0100

    yeserdays assignment

---
+++ fundamentals/assignments/Aliyu.py/strings assignment ans.py
@@ -0,0 +1,17 @@
+# Q1. Study all the string methods and list out your findings
+
+#capitalize:it captalizes a string that was formerly in lowercase
+place="abuja"
+print(place.capitalize())
+
+#casefold:it converts a string into a lower case
+name="TAHIR"
+print(name.casefold())
+
+#center
+function="python"
+print(function.center(14,"£"))
+
+#count
+country="america"
+print(country.count("m"))
