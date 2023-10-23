c658b96b349c5e24d590c06e73f9476a57c3c9cc
muhammad <muhammadnursanusialiyu05>
2023-10-23 11:37:23 +0100

    classwork4.py

---
+++ fundamentals/assignments/Aliyu.py/classwork4.py
@@ -0,0 +1,23 @@
+#1
+v1=[8]
+v2=[20]
+v3=[6]
+v4=[5]
+print(v1+v2+v3+v4)
+
+#2
+num=6
+num+=4
+print(num)
+
+#3
+print(v1==v2==v3==v4)
+
+#4
+list=["pears","mango","apple","burger"]
+print("mango" in list)
+
+#5
+print(v1 or v2)
+print(v3 and v4)
+print(not v3 or v4)
