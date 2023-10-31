1e3f3462eca811b648a8bffae500fcf7235b059e
Nasir Mustapha <teboy.com@gmail.com>
2023-10-31 11:48:38 +0100

    loop assignment

---
+++ fundamentals/classwork/classwork8.py
@@ -0,0 +1,9 @@
+# Q1. Write a football game where a user is prompted with a player
+#     skills rating and is asked to guess the player. If they guess right
+#     they get 5 points, if they guess wrong 3 points get deducted.
+#     At the end of the game, user is shown their performance.
+
+# Q2. Write a tailoring game where a user is asked to enter their
+#     dress information including length, width and neck size based
+#     on the users input, you show the user a list of fitting dresses
+#     from your dress collection.
