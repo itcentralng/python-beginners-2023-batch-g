eb35c544a7513111b7fed037d3d1e62919b8be29
Nasir Mustapha <teboy.com@gmail.com>
2023-11-02 12:16:59 +0100

    prime numbers assignment

---
+++ fundamentals/classwork/classwork10.py
@@ -0,0 +1,12 @@
+# Q1. Using while loop, loop through numbers 1 - 10 and
+# print out the result of multiplying each number by 3.
+
+for i in range(1, 11):
+    print(i*3)
+
+for i in range(1, 6):
+    print(i)
+
+# Q2. Write a code to find all prime numbers between 2 and 35 inclusive.
+
+
