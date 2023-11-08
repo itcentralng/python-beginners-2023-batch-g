89b1ce82180a5374a3532be378644ff635e99a3b
Nasir Mustapha <teboy.com@gmail.com>
2023-11-08 11:17:43 +0100

    update on function assignment

--- fundamentals/assignments/functions.py
+++ fundamentals/assignments/functions.py
@@ -1,6 +1,6 @@
 # Q1. Write a function that takes 3 numbers and prints the first number
-# as it's, the second number * 3 and the third number raised to the
-# power of 3. e.g. yourfunction(1, 2, 3) => 1, 6, 27
+# as it's, the second number multiplied by 3 and the third number raised
+#  to the power of 3. e.g. yourfunction(1, 2, 3) => 1, 6, 27
 
 # Q2. Write a function that takes a list of 6 numbers and return
 # all the even numbers in a new list 
