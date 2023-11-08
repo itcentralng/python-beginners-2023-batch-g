c1e07354631127cb3ad1770fc352fb166f4a02b9
Nasir Mustapha <teboy.com@gmail.com>
2023-11-08 11:14:45 +0100

    function assignment

---
+++ fundamentals/assignments/functions.py
@@ -0,0 +1,9 @@
+# Q1. Write a function that takes 3 numbers and prints the first number
+# as it's, the second number * 3 and the third number raised to the
+# power of 3. e.g. yourfunction(1, 2, 3) => 1, 6, 27
+
+# Q2. Write a function that takes a list of 6 numbers and return
+# all the even numbers in a new list
+
+# Q3. Write a function that takes a string of alphabets and prints out
+# all the vowels found in the string without repetition.
