061aae85b14664a14089a5d5d6aaa48e4ad57453
Nasir Mustapha <teboy.com@gmail.com>
2023-11-23 12:07:43 +0100

    problem solving

---
+++ revision/promblesolving.py
@@ -0,0 +1,20 @@
+# Q1. Create a program to manage a library's book lending system
+
+# - We are creating a library lending system for iT Central
+# - Add librarian:- name, id
+# - Add books - name, author, id, copies, genre
+# - Add students:- name, id, class, passport
+# - Store books borrowed:- book, student, date borrowed, return date
+# - Update borrowed books on return - add returned date
+
+# Usage:
+# - Librarian logs in to the system - with name and id
+# - Librarian adds books - with name, author, id, copies, genre
+# - Librarian adds students - with name, id, class, passport
+#  - A student picks a book and comes to the librarian:
+#           - Librarian enters - book, student, date borrowed, return date
+#           - system deducts from the copies of that book
+# - Student returns book borrowed
+# - Librarian updates borrowed book
+
+# Q2. Create a program to manage admission process in iT Central
