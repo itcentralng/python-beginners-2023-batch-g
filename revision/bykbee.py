7d1d6f2c3c1935125e78498d9833f2146f372607
kabiru abubakar <kabeeru26@gmail.com>
2023-11-27 12:13:20 +0100

    yes sir

--- revision/bykbee.py
+++ revision/bykbee.py
@@ -13,6 +13,15 @@
 #           -Java Programming
 #       -age
 #       -school
+#       -Waec/Neco
+#       -jamb scores
 #       -description
 #   -submit the form
-#-iTcentral add everyone that fill the forms
+#-iTcentral submit to cut of marks
+#       -you must have Waec/Neco 9 credit       
+#       -then you have more than 170 in jamb
+#               -iTcentral check if you have Waec/Neco 9 credit and jamb 170%
+#                    -print (congraturation you got admission)
+#               -else 
+#                    -print(You didn't get what you were looking for, sorry wait another year)   
+#
