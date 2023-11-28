f9a6bda9466687d47d3d020c827bbab85a912252
muhammad <muhammadnursanusialiyu05>
2023-11-28 09:46:12 +0100

    admission process by nur

---
+++ revision/slnbynur.py
@@ -0,0 +1,37 @@
+# Q2. Create a program to manage admission process in iT Central
+
+# - PREPARATION FOR EXAMINATION
+        #-Advertisement of the examination through socail media platforms
+                #-Instegram,twitter,facebook,radio and newspaper
+        #-Production of forms:Student's full name,[Parents:names,phone number,home address],Grade applying for,State of origin
+        #-Sales of the forms to aspirants
+        #-Exams question setting:
+                #-Questions should be set by teachers of the subjects:MATHS,ENGLISH,GENERAL KNOWNLEGE AND SCIENCE
+                #-The exam is obj which is computer based
+                #-duration 2hrs
+                #-Submission of questions to exams office
+# - Conduction of exams in the school's exam hall
+# - Scoring :Cut off mark should be set to 60 marks
+        #-If a student passes the cut off mark they should be added to the admission list
+# - Admission of students:
+        #-Acceptance letter should be sent to parents
+# - Registration/Screening of students :
+        #-Biometrics should be taken to add the student to the school's server
+        #-Admitted students should come along with the following documents :
+                #-Primary school leaving certificate
+                #-Acceptance letter
+                #-Admissin form
+#-Reg number should be asssigned to each student
+# - ID card should be made and contain :
+        #-Student's name
+        #-Reg number
+        #-Picture
+
+#- USAGE
+# - The school advertises its entrance exams through socail media platforms
+# - Production and sales of admission forms
+# - Setting exam questions by teachers and submission to exams office
+# - Conduct exams on aspirants by the exams office       
+# - If an aspirant passes the cut off mark an acceptance letter is sent through the parents emails
+# - Registration/screening of successful students
+ 
