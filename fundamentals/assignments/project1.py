80d4dd17bc394b5138cf1e7f2a19bcdd87c3c065
Nasir Mustapha <teboy.com@gmail.com>
2023-11-29 11:38:01 +0100

    Project 1 assignment

---
+++ fundamentals/assignments/project1.py
@@ -0,0 +1,5 @@
+# Create a simple flask app with the following endpoints:
+# / - this endpoint should tell the user what the app is all about, users should be
+# able to navigate to other pages from this endpoint.
+# /contact - users should learn about how to contact you on this endpoint
+# /people - users should see a list of the people behind the app.
