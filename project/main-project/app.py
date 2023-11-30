a308c2ee9a7c8a600091897ffacf7c13c4b10712
Nasir Mustapha <teboy.com@gmail.com>
2023-11-30 11:55:51 +0100

    working with session, flash, request and static files

--- project/main-project/app.py
+++ project/main-project/app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, session
 
 app = Flask(__name__)
 
+app.secret_key = "sdjfhsdkjfhsjdkhfjksdh"
+
+
+database = []
+
 @app.get("/")
 def index():
-    return render_template('home.html')
+    return render_template('register.html')
+
+@app.post("/")
+def index2():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    data = {"name":name, "email":email, "password":password, "id":len(database)+1, 'avatar':"{}.jpg".format(len(database)+1)}
+    database.append(data)
+    return redirect('/login')
+
+@app.get("/login")
+def login():
+    return render_template('login.html')
+
+@app.post("/login")
+def login2():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    for data in database:
+        if data.get('email') == email and data.get('password') == password:
+            session['user'] = data
+            return redirect('/dashboard')
+    flash("Wrong credentials, please try again!")
+    return render_template('login.html')
+
 @app.get('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
